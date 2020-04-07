f34047d173d81dd3a14075fed458e9c18d3eace9
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2020-04-07 17:15:55 +0530

    get posts

--- helper/fetchData.js
+++ helper/fetchData.js
@@ -1,10 +1,19 @@
 import client from '../core/contentful';
+import * as contentful from 'contentful';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import _ from 'lodash';
+
 
 export async function fetchPosts() {
   const entries = await client.getEntries({
-    content_type: "blogPost"
+    content_type: "post",
+    'fields.tag.sys.id': '6mZQPJodbURKfgSo4v9QiB'
+    // 'fields.tag.fields.tagName[match]': 'adroll'
+
+
   })
-  if (entries.items) return entries.items
+  if (entries) return entries
   console.log(`Error getting Entries for ${contentType.name}.`)
 }
 
@@ -14,9 +23,11 @@ export async function fetchImages() {
   const entries = await client.getEntries({
     content_type: "images"
   })
-  console.log(entries)
   if (entries.includes.Asset) return entries.includes.Asset
   console.log(`Error getting Entries for images.`)
 }
 
 
+export function getRenderedHTML (doc) {
+  return documentToReactComponents(doc)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import _ from 'lodash';
 
 import { siteDetails } from "../data/site-details";
-import { fetchPosts, fetchImages } from "../helper/fetchData";
+import { fetchPosts, fetchImages, getRenderedHTML } from "../helper/fetchData";
 const gridCell = {
   color: "white",
   padding: "30px",
@@ -27,15 +28,18 @@ function HomePage() {
   const [posts, setPosts] = useState([]);
   const [images, setImages] = useState([]);
   useEffect(() => {
-    async function getPosts() {
-      const allPosts = await fetchPosts();
-      setPosts([...allPosts]);
-    }
     async function getImages() {
       const allImages = await fetchImages();
       setImages([...allImages]);
     }
     getImages();
+  }, []);
+
+  useEffect(() => {
+    async function getPosts() {
+      const allPosts = await fetchPosts();
+      setPosts({ items: allPosts.items, includes: allPosts.includes });
+    }
     getPosts();
   }, []);
 
@@ -51,32 +55,45 @@ function HomePage() {
         />
       </Head>
       {/* {siteDetails[process.env.siteTitle]["title"]} */}
-      <img src={`${siteDetails[process.env.siteTitle].logo}`} alt="" />
-      {/* {posts.length > 0
-        ? posts.map((p) => <div key={p.fields.title}>{p.fields.title}</div>)
-        : null} */}
-<div>
-              <table style={tableStyle}>
-                <tbody>
-                  <tr>
-      {images.length > 0
-        ? images.map((p) => (
+      <div>
+        <div className="posts">
+          {posts.items && posts.items.length > 0
+            ? posts.items.map(p => {
+              console.log(p)
+              const renderedHtml = getRenderedHTML(p.fields.content);
+              return (
+                <div>
+                  {console.log(posts.includes)}
+                  <img src={`${siteDetails[process.env.siteTitle].logo}`} alt="" />
+                  <img src={`${p.fields.postImage.fields.file.url}`} alt="" />
+                  <h1>{p.fields.title}</h1>
+                  <div>{renderedHtml}</div>
+                </div>
+              )
+            })
+            : null}
+        </div>
+        <table style={tableStyle}>
+          <tbody>
+            <tr>
+              {images.length > 0
+                ? images.map((p) => (
 
-                    <td key={p.fields.title} style={gridCell}>
-                      <a href="#">
-                        <img
-                          style={gridlogo}
-                          src={p.fields.file.url}
-                          alt={p.fields.title}
-                        />
-                      </a>
-                    </td>
-          ))
-        : null}
-         </tr>
-         </tbody>
-      </table>
-    </div>
+                  <td key={p.fields.title} style={gridCell}>
+                    <a href="#">
+                      <img
+                        style={gridlogo}
+                        src={p.fields.file.url}
+                        alt={p.fields.title}
+                      />
+                    </a>
+                  </td>
+                ))
+                : null}
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
